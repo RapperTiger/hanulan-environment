2f4ee3baeddbdaef5d48ab5662f6d9adba9cfabd
RapperTiger <116620632+RapperTiger@users.noreply.github.com>
2024-06-13 12:00:35 +0900

    Update adminpage.py

--- adminpage.py
+++ adminpage.py
@@ -3,3 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+
+st.title('This is administator page')
+st.subheader('if you click your classroom then point will charge')
+st.markdown("---")
