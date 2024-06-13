cb65f98d43fcaa35165847b81f27dc36adc6b20b
RapperTiger <116620632+RapperTiger@users.noreply.github.com>
2024-06-13 11:45:59 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,6 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 
-st.title('한울안중학교 환경생태 프로젝트')
-st.subheader('탄소중립 포인트 모으기!')
+st.title('Hanulan Middle School Enviornment Project')
+st.subheader('Set aside your point!')
 st.markdown("---")
