f61158d99c7e62305bcd0f9ab3a554e68e82df6f
RapperTiger <116620632+RapperTiger@users.noreply.github.com>
2024-06-13 11:51:21 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit as st 
 import pandas as pd
 import numpy as np
-import matplotlib as plt
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 st.title('Hanulan Middle School Enviornment Project')
 st.subheader('Set aside your point!')
